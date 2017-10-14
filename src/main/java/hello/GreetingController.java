c8345ac58af7dbc175ed000586606b6f2b67e4ad
Andy Evans <andrewevans0@outlook.com>
2017-10-14 12:06:39 +0100

    cleaned out all of servi

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,9 +14,6 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        System.out.println("where is the native path supposed to be?");
-        System.out.println(System.getProperty("java.library.path"));
-        new HelloWorld().print();
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
--- src/main/java/hello/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-public class HelloWorld {
-    public native void print();
-    static {
-        System.loadLibrary("HelloWorld");
-    }
-}
