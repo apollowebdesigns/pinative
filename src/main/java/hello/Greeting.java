ccca652dc4f09e0c6722ac5a42d32c74282f5ddf
Andy Evans <andrewevans0@outlook.com>
2017-10-14 12:13:34 +0100

    testing out statics

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,10 +4,13 @@ public class Greeting {
 
     private final long id;
     private final String content;
+    static native void hello();
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
+        System.loadLibrary("HelloWorld");
+        hello();
     }
 
     public long getId() {
