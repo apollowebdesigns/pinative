87f09364f21a3bf008553af00a1bc5dfd5b2749b
Andy Evans <andrewevans0@outlook.com>
2017-10-14 10:07:58 +0100

    adding logging for locating library location

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -14,6 +14,8 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        System.out.println("where is the native path supposed to be?");
+        System.out.println(System.getProperty("java.library.path"));
         new HelloWorld().print();
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
